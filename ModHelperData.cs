dfd554a068866e4aad435e2fe8f64b8aad7220cc
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-07 10:06:16 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+﻿namespace Thingy
+{
+    public static class ModHelperData
+    {
+        public const string WorksOnVersion = "38.0";
+        public const string Version = "1.1";
+        public const string Name = "Banana Thrower";
+        public const string Description =
+            "Hello today i added a banana Thrower also check out my youtube channel @MattcyYt with blue and orange icon thanks! Thanks To Thingy For The Idea";
+
+        public const string RepoOwner = "Mattcy1";
+        public const string RepoName = "Thingy-Tower";
+        public const string Icon = "Thingy1.png";
+    }
+}
